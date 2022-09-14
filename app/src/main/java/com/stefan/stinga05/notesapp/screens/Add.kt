50f9d6f4327154efb8b62cd8042bd0abe2a18632
StefanmdaA3 <kdkwjsjs@mail.ru>
2022-09-14 15:48:52 +0300

    Lesson 1

---
+++ app/src/main/java/com/stefan/stinga05/notesapp/screens/Add.kt
@@ -0,0 +1,9 @@
+package com.stefan.stinga05.notesapp.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Add(navController: NavHostController) {
+
+}
---
+++ app/src/main/java/com/stefan/stinga05/notesapp/screens/Start.kt
@@ -0,0 +1,11 @@
+package com.stefan.stinga05.notesapp.screens
+
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Start(navController: NavHostController) {
+    Text(text = "Start Screen")
+}
